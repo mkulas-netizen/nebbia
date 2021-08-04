cf91bd72d4acaa8b44fc269bc3dedb71582888b5
mkulas <mkulas@bjs.sk>
2021-08-04 12:33:40 +0200

    FronEnd add target blank

--- resources/views/reader/index.blade.php
+++ resources/views/reader/index.blade.php
@@ -10,7 +10,7 @@
                         <div class="comment mt-5 shadow-lg border-dark p-3 text-justify"><img
                                 src="https://i.imgur.com/CFpa3nK.jpg" alt="" class="rounded-circle" width="40"
                                 height="40">
-                            <h4><a href="{{ $item->link }}"> {{ $item->title }}</a></h4>
+                            <h4><a href="{{ $item->link }}" target="_blank"> {{ $item->title }}</a></h4>
                             <h5>{{ $item->description }}</h5>
                             <span>- {{ $item->pubDate }} </span>
                         </div>
